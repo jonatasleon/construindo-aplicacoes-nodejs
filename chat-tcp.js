bb79aa534f4f8ba442525447ed5d59c310571400
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-04 09:19:11 -0300

    Add chat-tcp.js

---
+++ chat-tcp.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var net = require('net');
+var chatServer = net.createServer();
+var clientList = [];
+
+var removeClient = function(data) {
+  clientList.splice(clientList.indexOf(5), 1);
+};
+
+var broadcast = function(message, sender) {
+  clientList.forEach(function(client) {
+    if (sender !== client)
+      client.write(message);
+  });
+};
+
+chatServer.on('connection', function(client) {
+  client.write('Hi guest!\n');
+  clientList.push(client); 
+  client.on('data', function(data) {
+    broadcast(data, client);
+  });
+  client.on('error', function(err) {
+    console.log(err);
+  });
+  client.on('end', removeClient);
+});
+
+chatServer.listen(9000);
