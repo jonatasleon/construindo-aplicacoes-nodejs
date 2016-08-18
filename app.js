54d24a5c2212799ebc2f080d78eb437ce003d68a
Jonatas Leon <jonatas.leon@gmail.com>
2016-08-17 23:39:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res) {
+  console.log('Isso é um middleware');
+});
+
 app.get('/', function(req, res) {
   res.send('Hello');
 });
