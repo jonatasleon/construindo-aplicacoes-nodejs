12354c34cd5576d70306e8a0239f75b17c44fcff
Jonatas Leon <jonatas.leon@gmail.com>
2016-10-01 19:48:30 -0300

    Remove quebras de linha

--- models/StormtrooperModel.js
+++ models/StormtrooperModel.js
@@ -6,31 +6,26 @@ function StormtropperModel(mongo) {
 }
 
 StormtropperModel.prototype.find = (query, callback) => {
-  this.collection
-    .find(query, callback);
+  this.collection.find(query, callback);
 };
 
 StormtropperModel.prototype.findOne = (_id, callback) => {
   let query = {_id: this.mongo.ObjectId(_id)};
-  this.collection
-    .findOne(query, callback);
+  this.collection.findOne(query, callback);
 };
 
 StormtropperModel.prototype.create = (data, callback) => {
-  this.collection
-    .insert(data, callback);
+  this.collection.insert(data, callback);
 };
 
 StormtropperModel.prototype.update = (_id, data, callback) => {
   let query = {_id: this.mongo.ObjectId(_id)};
-  this.collection
-    .update(query, data, callback);
+  this.collection.update(query, data, callback);
 };
 
 StormtropperModel.prototype.remove = (_id, callback) => {
   let query = {_id: this.mongo.ObjectId(_id)};
-  this.collection
-    .remove(query, callback);
+  this.collection.remove(query, callback);
 };
 
 module.exports = (mongo) => new StormtropperModel(mongo);
